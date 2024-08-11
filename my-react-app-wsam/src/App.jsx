690b1ba6a31a62b94b8f384c22bb2da896c3b9f5
Alex <alexjstewart220@gmail.com>
2024-08-11 16:43:28 -0400

    practice with conditional rendering

--- my-react-app-wsam/src/App.jsx
+++ my-react-app-wsam/src/App.jsx
@@ -1,14 +1,11 @@
-import Student from './Student.jsx'
+import UserGreeting from "./UserGreeting.jsx"
 
 function App() {
 
   return (
     <>
-    <Student name="Spongebob" age={'30'} isStudent={true}/>
-    <Student name="Patrick" age={42} isStudent={false}/>
-    <Student name="Squidward" age={50} isStudent={false}/>
-    <Student name="Sandy" age={27} isStudent={true}/>
-    <Student name="Larry"/>
+ <UserGreeting isLoggedIn={true} username="AlexJStewart"/>
+
     </>
   )
 }
