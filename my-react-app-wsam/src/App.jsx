a2851e2d9802aea914908a57b3b48087efc32360
Alex <alexjstewart220@gmail.com>
2024-08-11 15:45:11 -0400

    working on styling with css

--- my-react-app-wsam/src/App.jsx
+++ my-react-app-wsam/src/App.jsx
@@ -1,15 +1,9 @@
-import Card from './Card.jsx'
-import Spencer from './assets/spencer.jpg'
-import Alex from './assets/alexheadshot2020.jpg'
-import HotDog from './assets/alexhotdog.jpeg'
+import Button from './Button.jsx'
+
 function App() {
 
   return (
-    <div>
-    <Card image={Alex}/>
-    <Card image={Spencer}/>
-    <Card image={HotDog}/>
-    </div>
+    <Button />
   )
 }
 
