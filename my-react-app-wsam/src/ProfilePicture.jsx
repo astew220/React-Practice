9df1d06a40b86eea297ef05cbe21ec870a17071b
Alex <alexjstewart220@gmail.com>
2024-08-13 15:56:04 -0400

    switching images

--- my-react-app-wsam/src/ProfilePicture.jsx
+++ my-react-app-wsam/src/ProfilePicture.jsx
@@ -2,10 +2,17 @@
 function ProfilePicture(){
 
 const imageUrl = './src/assets/alexhotdog.jpeg'
-
+const imageUrl2 = './src/assets/profilepic.jpeg'
 const handleClick = (e) => {
-   e.target.style.border = '1px solid red'
+    if(e.target.src.endsWith('hotdog.jpeg')){
+        console.log('it worked')
+        e.target.src = imageUrl2
+    } else {
+        e.target.src = imageUrl;
+    }
 }
+  
+
 
     return(<img onClick={handleClick} src={imageUrl}/>)
 
